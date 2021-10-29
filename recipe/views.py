e091d7e0feb91c3fca88fda74e6e1626a882158b
Bolaji Olawale <bolaji.olawale007@gmail.com>
2021-10-29 18:41:31 +0100

    Register application

--- recipe/views.py
+++ recipe/views.py
@@ -8,5 +8,5 @@ def explore_recipe(request):
     ingredients = ["Ball pepper", "Tomatoes", "Spinach"]
     recipe = generate_recipe(ingredients=ingredients)
 
-    context_data = recipe
+    context_data = {"recipes": recipe}
     return render(request, 'recipe/index.html', context=context_data)
