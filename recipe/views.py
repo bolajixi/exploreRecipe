f28419fdd83e141492a1040c0f81f018be33b073
Bolaji Olawale <bolaji.olawale007@gmail.com>
2021-10-29 17:46:49 +0100

    Add explore recipe view

--- recipe/views.py
+++ recipe/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .recipe_generator import generate_recipe
 
 # Create your views here.
+
+
+def explore_recipe(request):
+    ingredients = ["Ball pepper", "Tomatoes", "Spinach"]
+    recipe = generate_recipe(ingredients=ingredients)
+
+    print(recipe)
+    # return render(request, 'shop/product/list.html', context_data)
