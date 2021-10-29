4ca4adf10b19df99595a497021b2faa03f301f84
Bolaji Olawale <bolaji.olawale007@gmail.com>
2021-10-29 17:13:40 +0100

    Add recipe generator

--- recipe/recipe-generator.py
+++ recipe/recipe-generator.py
@@ -1,17 +1,27 @@
 import os
 import openai
+from django.conf import settings
 
 
-def generate_recipe(**kwargs):
+def generate_recipe(ingridents, **kwargs):
     """
     Generate a recipe given a list of ingridents
     """
     openai.api_key = os.getenv(
-        "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")
+        settings.OPENAI_SECRET_KEY)
+
+    prompt_list = [
+        "Write a recipe based on these ingredients and instructions:"]
+
+    for ingrident in ingridents:
+        prompt_list.append("\n"+ingrident)
+
+    prompt = "".join(prompt_list)
 
     response = openai.Completion.create(
         engine="davinci-instruct-beta",
-        prompt="Write a recipe based on these ingredients and instructions:\n\nFrito Pie\n\nIngredients:\nFritos\nChili\nShredded cheddar cheese\nSweet white or red onions, diced small\nSour cream\n\nDirections:",
+        # prompt="Write a recipe based on these ingredients and instructions:\n\nFrito Pie\n\nIngredients:\nFritos\nChili\nShredded cheddar cheese\nSweet white or red onions, diced small\nSour cream\n\nDirections:",
+        prompt=prompt_list,
         temperature=0,
         max_tokens=120,
         top_p=1.0,
