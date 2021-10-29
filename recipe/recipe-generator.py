69f17ff4fd81316f0c925244d39c42e49e9eddb7
Bolaji Olawale <bolaji.olawale007@gmail.com>
2021-10-29 17:36:45 +0100

    Add ingredients to prompt

--- recipe/recipe-generator.py
+++ recipe/recipe-generator.py
@@ -3,28 +3,33 @@ import openai
 from django.conf import settings
 
 
-def generate_recipe(ingridents, **kwargs):
+def generate_recipe(ingredients, **kwargs):
     """
     Generate a recipe given a list of ingridents
+
     """
     openai.api_key = os.getenv(
         settings.OPENAI_SECRET_KEY)
 
-    prompt_list = [
-        "Write a recipe based on these ingredients and instructions:"]
+    prompt_sentence = [
+        "Write a recipe based on these ingredients and instructions:\n\nIngredients:"]
 
-    for ingrident in ingridents:
-        prompt_list.append("\n"+ingrident)
+    for ingredient in ingredients:
+        prompt_sentence.append("\n"+ingredient)
 
-    prompt = "".join(prompt_list)
+    prompt_sentence.append("\n\nDirections:")
+    new_prompt = "".join(prompt_sentence)
 
     response = openai.Completion.create(
         engine="davinci-instruct-beta",
         # prompt="Write a recipe based on these ingredients and instructions:\n\nFrito Pie\n\nIngredients:\nFritos\nChili\nShredded cheddar cheese\nSweet white or red onions, diced small\nSour cream\n\nDirections:",
-        prompt=prompt_list,
+        prompt=new_prompt,
         temperature=0,
         max_tokens=120,
         top_p=1.0,
         frequency_penalty=0.0,
         presence_penalty=0.0
     )
+
+    res = response.to_dict_recursive()
+    return res["choices"][0]["text"]
