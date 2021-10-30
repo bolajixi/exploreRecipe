783689036ec780a67f8c5aee835db62101f916d0
Tofunmi Adigun-Hameed <tofunmi.ah@gmail.com>
2021-10-30 10:47:04 +0100

    adding other meal ideas

--- recipe/views.py
+++ recipe/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
-from .recipe_generator import generate_recipe
+from .recipe_generator import generate_recipe, other_meal_ideas
 
 # Create your views here.
 
 
 def explore_recipe(request):
-    ingredients = ["Ball pepper", "Tomatoes", "Spinach"]
+    ingredients1 = ["Ball pepper", "Tomatoes", "Spinach"]
+    ingredients = ["new potatoes", "oil",
+                   "leeks", "eggs", "pepper smoked mackerel", "creamed horseradish"]
     recipe = generate_recipe(ingredients=ingredients)
+    other_ideas = other_meal_ideas(ingredients)
 
-    context_data = {'recipe': recipe}
-    return render(request, 'recipe/index.html', context=context_data)
+    context_data = {'recipe': recipe,
+                    'other_ideas': other_ideas,
+                    'ingredients': ingredients,
+                    }
+    return render(request, 'recipe/generated_recipe.html', context=context_data)
